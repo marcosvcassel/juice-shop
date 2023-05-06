2e6243ea2d6e145b2121618e9699237f62dffcca
marcos.cassel <marcos.cassel@powerschool.com>
2023-05-06 08:54:14 -0700

    branch-7 - Iac branch

---
+++ terraform/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+
+  backend "s3" {
+    bucket = "juice-shop-terraform-state"
+    key    = "terraform"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
